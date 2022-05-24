c57d903f7c900531de5ab8edf407135bee4865f9
leofab <leofabr3@gmail.com>
2022-05-24 20:17:41 -0300

    add script

--- modulo-ii-fundamentos-web/script.js
+++ modulo-ii-fundamentos-web/script.js
@@ -1,1 +1,18 @@
 'use strict'
+
+const input = document.getElementById('input');
+const btn = document.getElementsByTagName('button')[0];
+
+btn.addEventListener('click', function(e) {
+    console.log('works');
+});
+
+input.addEventListener('input', function(e) {
+    e.preventDefault();
+    // if(input.textContent.length === 0) {
+    //     console.log('works');
+    // }
+    console.log('works');
+    btn.disabled = false;
+    btn.style.backgroundColor = 'hsla(208, 83%, 73%, 0.965)';
+});
