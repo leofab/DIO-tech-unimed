81fc1f85fe867e630fc8a008e90d6c01746b21ee
leofab <leofabr3@gmail.com>
2022-05-26 18:20:19 -0300

    angular init

---
+++ modulo-iv-web-angular/course-manager/src/app/courses/course-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
+
+@Component({
+    selector: "app-course-list",
+    templateUrl: "./course-list.component.html",
+})
+export class CourseListComponent implements OnInit {
+
+    courses: Course[] = [];
+
+    ngOnInit(): void {
+        this.courses = [
+            {
+            id: 1,
+            name: 'Angular Form',
+            imageURL: '',
+            price: 99.99,
+            code: 'XPS-8793',
+            duration: 120,
+            rating: 4.9,
+        },
+        {
+            id: 2,
+            name: 'Angular HTTP',
+            imageURL: '',
+            price: 45.99,
+            code: 'XPS-8792',
+            duration: 80,
+            rating: 4,
+        }
+    ]
+    }
+
+}
---
+++ modulo-iv-web-angular/course-manager/src/app/courses/course.ts
@@ -0,0 +1,9 @@
+export class Course {
+    id?: number;
+    name?: string;
+    imageURL?: string;
+    price?: number;
+    code?: string;
+    duration?: number;
+    rating?: number;
+}
