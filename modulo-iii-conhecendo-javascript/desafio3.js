f941dd895e29d23e6d8f69e7be9bb1d82b41d543
leofab <leofabr3@gmail.com>
2022-05-26 08:57:07 -0300

    desafio 3

--- modulo-iii-conhecendo-javascript/desafio3.js
+++ modulo-iii-conhecendo-javascript/desafio3.js
@@ -3,6 +3,15 @@ let botaoAtualizar = document.getElementById('atualizar-saldo');
 let botaoLimpar = document.getElementById('limpar-saldo');
 let soma = document.getElementById('soma');
 let campoSaldo = document.getElementById('campo-saldo');
+function somarAoSaldo(saldo, valor) {
+    campoSaldo.innerHTML = String(Number(soma.value) + Number(campoSaldo.innerText));
+}
+function limparSaldo() {
+    campoSaldo.innerHTML = '0';
+}
 botaoAtualizar.addEventListener('click', function () {
-    console.log(soma.value);
+    somarAoSaldo(Number(campoSaldo.innerText), Number(soma.value));
+});
+botaoLimpar.addEventListener('click', function () {
+    limparSaldo();
 });
--- modulo-iii-conhecendo-javascript/desafio3.ts
+++ modulo-iii-conhecendo-javascript/desafio3.ts
@@ -3,7 +3,18 @@ let botaoLimpar = document.getElementById('limpar-saldo') as HTMLButtonElement;
 let soma = document.getElementById('soma') as HTMLInputElement;
 let campoSaldo = document.getElementById('campo-saldo') as HTMLSpanElement;
 
-botaoAtualizar.addEventListener('click', function () {
-    console.log(soma.value);
+function somarAoSaldo(saldo: number, valor: number) {
+    campoSaldo.innerHTML = String(Number(soma.value) + Number(campoSaldo.innerText));
+}
 
+function limparSaldo () {
+    campoSaldo.innerHTML = '0';
+}
+
+botaoAtualizar.addEventListener('click', function () {
+    somarAoSaldo(Number(campoSaldo.innerText), Number(soma.value));
+});
+
+botaoLimpar.addEventListener('click', function () {
+    limparSaldo();
 })
