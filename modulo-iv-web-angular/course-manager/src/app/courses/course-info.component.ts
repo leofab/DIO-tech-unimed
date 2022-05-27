8bb456b12021179a1f9d7d74c4430c80340f595c
leofab <leofabr3@gmail.com>
2022-05-27 18:52:06 -0300

    modulo iv save refact in component html

--- modulo-iv-web-angular/course-manager/src/app/courses/course-info.component.ts
+++ modulo-iv-web-angular/course-manager/src/app/courses/course-info.component.ts
@@ -16,12 +16,13 @@ export class CourseInfoComponent implements OnInit{
     ngOnInit(): void {
       this.courseService.retrieveById(Number(this.activatedRoute.snapshot.paramMap.get('id'))).subscribe({
           next: course => this.course = course,
-          error: err => console.error('Error', err)
+          error: err => console.log('Error', err)
       });
   }
 
   save(): void {
-      this.courseService.save(this.course!).subscribe({
+      this.courseService.save(this.course!)
+      .subscribe({
           next: course => console.log('Saved with sucess', course),
           error: err => console.log('Error', err)
       })
