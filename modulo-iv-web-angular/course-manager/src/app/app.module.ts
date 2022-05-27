0d2b7e01125765b9605c6b0ef123684baf54642c
leofab <leofabr3@gmail.com>
2022-05-27 13:17:24 -0300

    modulo iv routes add

--- modulo-iv-web-angular/course-manager/src/app/app.module.ts
+++ modulo-iv-web-angular/course-manager/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/star.component';
 import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +15,19 @@ import { ReplacePipe } from './pipe/replace.pipe';
     CourseListComponent,
     StarComponent,
     ReplacePipe,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: 'courses', component: CourseListComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
