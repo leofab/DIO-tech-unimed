0125ce9830bf9c3504a589ae316f09e278d4dec8
leofab <leofabr3@gmail.com>
2022-05-27 10:35:35 -0300

    modulo iv correct type assertions errors

--- modulo-iv-web-angular/course-manager/src/app/star/star.component.ts
+++ modulo-iv-web-angular/course-manager/src/app/star/star.component.ts
@@ -7,9 +7,9 @@ import { Component, Input, OnChanges } from "@angular/core";
 })
 export class StarComponent implements OnChanges{
     @Input()
-    rating: number = 0;
-    starWidth: number = 0;
+    rating?: number;
+    starWidth?: number;
     ngOnChanges(): void {
-        this.starWidth = this.rating * 74 / 5;
+        this.starWidth = (this.rating as number) * 74 / 5;
     }
 }
