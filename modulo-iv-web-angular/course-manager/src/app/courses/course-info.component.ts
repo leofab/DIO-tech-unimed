9922c8c45d4bd52b1f956ccded60af9529f97b6f
leofab <leofabr3@gmail.com>
2022-05-30 09:06:10 -0300

    modulo iv save still not working

--- modulo-iv-web-angular/course-manager/src/app/courses/course-info.component.ts
+++ modulo-iv-web-angular/course-manager/src/app/courses/course-info.component.ts
@@ -21,8 +21,7 @@ export class CourseInfoComponent implements OnInit{
   }
 
   save(): void {
-      this.courseService.save(this.course!)
-      .subscribe({
+      this.courseService.save(this.course!).subscribe({
           next: course => console.log('Saved with sucess', course),
           error: err => console.log('Error', err)
       })
