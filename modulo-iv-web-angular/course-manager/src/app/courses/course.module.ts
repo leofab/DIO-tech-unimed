261d06650b9c26f33440b3035f8cce7bf5007e28
leofab <leofabr3@gmail.com>
2022-05-30 10:29:49 -0300

    modulo iv error404 segregation

--- modulo-iv-web-angular/course-manager/src/app/courses/course.module.ts
+++ modulo-iv-web-angular/course-manager/src/app/courses/course.module.ts
@@ -25,6 +25,7 @@ import { CourseListComponent } from "./course-list.component";
             {
                 path: "courses/info/:id", component: CourseInfoComponent
             }
+            
         ])
     ],
 })
