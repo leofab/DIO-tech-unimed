8b8cccf00598c4e58c39f884a276c50b7c20ffbc
leofab <leofabr3@gmail.com>
2022-05-31 12:09:11 -0300

    spa page toolbar component add

--- modulo-iv-web-angular/spa-page/src/app/app.module.ts
+++ modulo-iv-web-angular/spa-page/src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ modulo-iv-web-angular/spa-page/src/app/toolbar/toolbar.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.css'],
+  inputs: ['title']
+})
+export class ToolbarComponent implements OnInit {
+
+  @Input() titleTool: string = '';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
